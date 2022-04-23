9d343e674a8a09da67e234cc500971ed8c803ac2
Federico Galan <fegv.2@hotmail.com.ar>
2022-04-23 11:50:57 -0300

    Se instala react router y se aplica a la barra de navegacion

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { DcScreen } from '../components/dc/DcScreen'
+import { LoginScreen } from '../components/login/LoginScreen'
+import { MarvelScreen } from '../components/marvel/MarvelScreen'
+import { SearchScreen } from '../components/search/SearchScreen'
+import {Navbar} from '../components/ui/Navbar'
+
+export const AppRouter = () => {
+  return (
+    <>
+        <BrowserRouter>
+        <Navbar />
+            <Routes>
+                <Route path="/" element={<LoginScreen />} />
+                <Route path="/marvel" element={<MarvelScreen />} />
+                <Route path="/dc" element={<DcScreen />} />
+                <Route path="/search" element={<SearchScreen />} />
+                <Route path="/login" element={<LoginScreen />} />
+            </Routes>
+        </BrowserRouter>
+    </>
+  )
+}
