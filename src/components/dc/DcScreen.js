0106750a4a5698655d1667bb28d659ee026db6c9
Federico Galan <fegv.2@hotmail.com.ar>
2022-04-23 16:47:15 -0300

    Se imprimen lista de heroes en componentes

--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { HeroList } from '../hero/HeroList'
 
 export const DcScreen = () => {
   return (
     <>
         <h1>Dc Screen</h1>
+        <HeroList publisher={'DC Comics'}/>
     </>
   )
 }
--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { HeroList } from '../hero/HeroList';
 
 export const MarvelScreen = () => {
   return (
     <>
         <h1>Marvel Screen</h1>
+        <HeroList publisher='Marvel Comics'/>
     </>
   )
 }
