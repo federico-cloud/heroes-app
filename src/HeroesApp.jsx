fabe1a09a33ae34a2274279857b980e92a16b366
Federico Galan <fegv.2@hotmail.com.ar>
2022-04-28 15:52:47 -0300

    SE agrega el reducer y el context provider a la app

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { AuthContext } from './components/auth/authContext';
+import { authReducer } from './components/auth/authReducer';
 import { AppRouter } from './routers/AppRouter';
 
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || {logged: false};
+}
+
 export const HeroesApp = () => {
+
+  const [user, dispatch] = useReducer(authReducer, {}, init);
+
   return (
     <>
-      <AppRouter/>
+      <AuthContext.Provider value={{
+        user,
+        dispatch
+      }}>
+        <AppRouter/>
+      </AuthContext.Provider>
     </>
   )
 }
