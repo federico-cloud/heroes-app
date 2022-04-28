8bdf3eeda1025e762db7a66aa9f6e45a523ea33c
Federico Galan <fegv.2@hotmail.com.ar>
2022-04-28 16:12:52 -0300

    Se realiza el grabado de datos en el localStorage y el login del usuario

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { types } from '../types/types';
+import {AuthContext} from '../auth/authContext';
 
 export const LoginScreen = () => {
 
   const navigate = useNavigate();
+  const {dispatch} = useContext(AuthContext);
   
   const handleLogin = () => {
+    
+    const action = {
+      type: types.login,
+      payload:{
+        name: 'Federico'
+      }
+    }
+    dispatch(action);
+    
     navigate('/marvel', {
       replace: true
     });
   }
-
+  
   
   return (
     <>
