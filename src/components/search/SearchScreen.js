1fba3743a626724ccef9602f9efb72824153317a
Federico Galan <fegv.2@hotmail.com.ar>
2022-04-28 10:55:47 -0300

    Se modifica el componente SearchScreen

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
 
 export const SearchScreen = () => {
+
+  const [formValues, handleInputChange, reset] = useForm({
+    search: ''
+  });
+
+  const {search} = formValues;
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    reset();
+  }
+
   return (
     <>
-        <h1>Search screen</h1>
+      <div className='row'>
+        <div className='col-5'>
+          <h4>Buscar heroe</h4>
+
+          <form onSubmit={handleSearch}>
+            <input 
+              type='text'
+              placeholder='Buscar...'
+              className='form-control'
+              name='search'
+              autoComplete='off'
+              onChange={handleInputChange}
+              value={search}
+            />
+
+            <button
+              className='btn btn-outline-primary mt-1'
+              type='submit'
+            >
+              Buscar
+            </button>
+
+          </form>
+
+        </div>
+      </div>
     </>
   )
 }
