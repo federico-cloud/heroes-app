26c7e162dfde2d46147536f037bbb478cb7550d6
Federico Galan <fegv.2@hotmail.com.ar>
2022-04-28 11:59:00 -0300

    Se agrega la busqueda de heroe por nombre en el componente

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,23 +1,37 @@
 import React from 'react'
-import { useForm } from '../../hooks/useForm'
+import queryString from 'query-string';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import { getHeroeByName } from '../../selectors/getHeroeByName';
+import {HeroCard} from '../hero/HeroCard';
+
 
 export const SearchScreen = () => {
 
-  const [formValues, handleInputChange, reset] = useForm({
-    search: ''
+  const navigate = useNavigate();
+  const location = useLocation();
+  const {q = '' }= queryString.parse(location.search);
+
+  const [formValues, handleInputChange] = useForm({
+    search: q
   });
 
   const {search} = formValues;
 
+  const heroesFiltered = getHeroeByName(q);
+
   const handleSearch = (e) => {
     e.preventDefault();
-    reset();
+    navigate(`?q=${search}`);
+
   }
 
   return (
     <>
       <div className='row'>
+      
         <div className='col-5'>
+
           <h4>Buscar heroe</h4>
 
           <form onSubmit={handleSearch}>
@@ -30,15 +44,28 @@ export const SearchScreen = () => {
               onChange={handleInputChange}
               value={search}
             />
-
             <button
               className='btn btn-outline-primary mt-1'
               type='submit'
             >
               Buscar
             </button>
-
           </form>
+
+        </div>
+
+        <div className='col-7'>
+        
+          <h4>Resultados</h4>
+          
+          {
+            heroesFiltered.map( heroe => (
+              <HeroCard
+                key={heroe.id}
+                {...heroe}
+              />
+            )) 
+          }
 
         </div>
       </div>
