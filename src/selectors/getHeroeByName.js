b077ee4f0a155de7f4c6b6987b2080f9cd79fe85
Federico Galan <fegv.2@hotmail.com.ar>
2022-04-28 11:57:27 -0300

    Se agrega el helper getHeroByName

---
+++ src/selectors/getHeroeByName.js
@@ -0,0 +1,16 @@
+import {heroes} from '../data/heroes';
+
+export const getHeroeByName = ( name = '' ) => {
+    
+    name = name.toLowerCase();
+
+    if(name === '') {
+        return [];
+    }
+
+    let heroesFilter = heroes.filter( heroe => heroe.superhero.toLowerCase().includes( name ) ); 
+
+    return heroesFilter;
+    
+
+}
