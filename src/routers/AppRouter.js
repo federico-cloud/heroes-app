98d5c3baa15214addd954070f49807dcf35a08ae
Federico Galan <fegv.2@hotmail.com.ar>
2022-04-29 09:31:16 -0300

    Se agregan las rutas publicas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,14 +3,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashBoardRoutes } from './DashBoardRoutes';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
   return (
     <>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LoginScreen />} />
-                <Route path="/login" element={<LoginScreen />} />
+
+                <Route path='/login' element={
+                  <PublicRoute>
+                    <LoginScreen />
+                  </PublicRoute>
+                }>
+                </Route>
 
                 <Route path="/*" 
                   element={
---
+++ src/routers/PublicRoute.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../components/auth/authContext';
+
+export const PublicRoute = ({children}) => {
+
+    const {user} = useContext(AuthContext);
+
+  return user.logged
+    ? <Navigate to="/marvel" />
+    :  children
+}
